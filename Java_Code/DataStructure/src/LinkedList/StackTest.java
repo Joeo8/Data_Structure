50855b1cf3add030971e6a9aa301871efe91ab28
Joeo8 <joe_ob@qq.com>
2021-01-23 20:44:46 +0800

    StackTest

---
+++ Java_Code/DataStructure/src/LinkedList/StackTest.java
@@ -0,0 +1,26 @@
+package LinkedList;
+
+import java.util.Stack;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Joeo8
+ * Time: 20:28
+ * Description: No Description
+ */
+public class StackTest {
+    //显示栈Stack的基本使用
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        //入栈
+        stack.add("张三");
+        stack.add("李四");
+        stack.add("王五");
+        //出栈
+        while(stack.size() > 0){
+            //pop将栈顶的数据取出
+            System.out.println(stack.pop());
+        }
+    }
+
+}
