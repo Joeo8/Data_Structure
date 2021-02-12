a6858aab31957048f91349ae4e67ae8def1dd459
Joeo8 <joe_ob@qq.com>
2021-02-12 20:14:37 +0800

    PolandNotation

--- Java_Code/DataStructure/src/Stack/PolandNotation.java
+++ Java_Code/DataStructure/src/Stack/PolandNotation.java
@@ -12,18 +12,51 @@ import java.util.Stack;
  */
 public class PolandNotation {
     public static void main(String[] args) {
+
+        //完成将一个中缀表达式转换成后缀表达式
+        /*
+         *思路:
+         *   将String类型的运算表达式转换成对应的List
+         */
+        String expression = "1+((2+3)*4)-5";
+        List<String> list = toInfixExpressionList(expression);
+        System.out.println(list);
         //先定义一个逆波兰表达式
         //(3+4)*5-6 ==> 3 4 + 5 * 6 -
         //为了方便查看,逆波兰表达式中,数字和符合用空格隔开
-        String suffixExpression = "3 4 + 5 * 6 - ";
-        /*思路:
+        /*String suffixExpression = "3 4 + 5 * 6 - ";
+        *//*思路:
          *   1.先将“3 4 + 5 * 6 - ”放到ArrayList中
          *   2.将ArrayList作为参数传递给方法,在遍历的过程中集合stack完成计算
-         */
+         *//*
         List<String> rpnList = getListString(suffixExpression);
         System.out.println(rpnList);
         int res = calculate(rpnList);
-        System.out.println(suffixExpression+"逆波兰计算结果为 ==》"+res);
+        System.out.println(suffixExpression + "逆波兰计算结果为 ==》" + res);*/
+    }
+
+    //写一个将中缀表达式转换成后缀表达式的方法
+    public static List<String> toInfixExpressionList(String s) {
+        //定义一个List存放中缀表达式中的内容
+        List<String> ls = new ArrayList<>();
+        int i = 0;     //指针,用于遍历中缀表达式
+        String str;    //对多位数进行拼接
+        char c;        //遍历到字符时,加入到c中
+        do {
+            //如果c不是一个数字,则加入到ls中
+            if ((c = s.charAt(i)) < 48 || (c = s.charAt(i)) > 57) {
+                ls.add("" + c);
+                i++;
+            } else {      //如果是数字,则需要考虑多位数的情况(拼接)
+                str = ""; //将str置空
+                while (i < s.length() && (c = s.charAt(i)) >= 48 && (c = s.charAt(i)) <= 57) {
+                    str += c; //拼接
+                    i++;
+                }
+                ls.add(str);
+            }
+        } while (i < s.length());
+        return ls;
     }
 
     //写一个方法单个取出suffixExpression表达式中的元素
@@ -73,7 +106,7 @@ public class PolandNotation {
                 } else {
                     throw new RuntimeException("操作符有误！");
                 }
-                stack.push(""+res );
+                stack.push("" + res);
             }
         }
         return Integer.parseInt(stack.pop());
