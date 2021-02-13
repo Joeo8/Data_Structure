97020917fe76ba848617272cddebd8c3b8efcffc
Joeo8 <joe_ob@qq.com>
2021-02-13 15:00:36 +0800

    Maze

--- Java_Code/DataStructure/src/Recursion/Maze.java
+++ Java_Code/DataStructure/src/Recursion/Maze.java
@@ -32,7 +32,48 @@ public class Maze {
             }
             System.out.println();
         }
+        setWay(map,1,1);
+        System.out.println("====================Over=====================");
+         for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
 
     }
 
+    //使用递归回溯走出迷宫
+    /*说明:
+     *   map表示地图
+     *   i,j表示起始位 (1,1)
+     *   如果能到达map[6][5],则说明通路已经找到
+     *   约定:map[i][j]的状态值为0表示没有走过;为1表示墙体;为2表示通路可以走;为3表示死路
+     *   最后设置策略 下 --> 右 --> 上 --> 左
+     *
+     * */
+    public static boolean setWay(int[][] map, int i, int j) {
+        if (map[6][5] == 2) {  //通路已经找到,Game Over
+            return true;
+        } else {
+            if (map[i][j] == 0) {  //如果这个点还没有走
+                map[i][j] = 2;  //设想可以走通
+                if (setWay(map, i + 1, j)) {            //向下走
+                    return true;
+                } else if (setWay(map, i, j + 1)) {     //向右走
+                    return true;
+                } else if (setWay(map, i - 1, j)) {     //向上走
+                    return true;
+                } else if (setWay(map, i, j - 1)) {     //向左走
+                    return true;
+                }else{
+                    //说明该点走不通是死路
+                    map[i][j] =3 ;
+                    return false;
+                }
+            }else{  //如果这个点已经走过
+                return false;
+            }
+        }
+    }
 }
