7afe5bde744ba28a3abf363b7e04c2c64c207aa5
Joeo8 <joe_ob@qq.com>
2021-02-20 03:16:34 +0800

    ShellSort

--- Java_Code/DataStructure/src/Sorting/ShellSort.java
+++ Java_Code/DataStructure/src/Sorting/ShellSort.java
@@ -1,8 +1,10 @@
 package Sorting;
 
-///希尔排序(交换式)
+///希尔排序(缩小增量排序)
 //将原始数组细分为多个组,在各个组中插入排序,并一次合并大小组,直至剩下一个组(即整个原始数组)
-//80000数据测试耗时10s
+//交换式80000数据测试耗时10s
+//移位式80000数据测试耗时1s
+
 import java.util.Arrays;
 
 /**
@@ -19,7 +21,8 @@ public class ShellSort {
             arrs[i] = (int) (Math.random() * 80000);
         }
         long start = System.currentTimeMillis();
-        shellSort(arrs);
+//        shellSort(arrs);
+        shellSort2(arrs);
         long end = System.currentTimeMillis();
         System.out.println("SelectSorting共耗时:" + (end - start) + "ms");
     }
@@ -27,11 +30,12 @@ public class ShellSort {
     public static void shellSort(int[] arr) {
         int temp = 0;
         int count = 0;
-        //将原始数组细分为多个小组,gap代表步长,也带着这组数
+        //增量gap,并逐步缩小增量(缩小增量排序)
+        //将原始数组细分为多个小组,gap代表步长,也代表组数(增量)
         for (int gap = arr.length / 2; gap > 0; gap /= 2) {
             //针对组内的数值进行插入排序
             for (int i = gap; i < arr.length; i++) {
-                //将组内的数据利用步长进行排序
+                //将组内的数据利用步长进行排序(交换式,可以当作是冒泡)
                 for (int j = i - gap; j >= 0; j -= gap) {
                     if (arr[j] > arr[j + gap]) {
                         temp = arr[j];
@@ -44,5 +48,24 @@ public class ShellSort {
         }
         System.out.println("========================ShellSort============================");
         System.out.println(Arrays.toString(arr));
+    }
+
+    //移步式希尔排序(高效！！！)
+    public static void shellSort2(int[] arr) {
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            //从第gap个元素,逐个对其所在的组进行直接插入排序(移动式)
+            for (int i = gap; i < arr.length; i++) {
+                int j = i;
+                int temp = arr[j];
+                while (j - gap >= 0 && temp < arr[j - gap]) {
+                    arr[j] = arr[j - gap];
+                    j -= gap;
+                }
+                arr[j] = temp;
+            }
+        }
+        System.out.println("========================ShellSort============================");
+        System.out.println(Arrays.toString(arr));
+
     }
 }
