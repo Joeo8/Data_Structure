b3535bac645eff67432015106b7494e8e9111ca6
Joeo8 <joe_ob@qq.com>
2021-02-20 12:48:59 +0800

    QuickSort

--- Java_Code/DataStructure/src/Sorting/QuickSort.java
+++ Java_Code/DataStructure/src/Sorting/QuickSort.java
@@ -1,5 +1,6 @@
 package Sorting;
-
+//快速排序
+//80000数据进行测试耗时60ms
 import java.util.Arrays;
 
 /**
@@ -10,8 +11,9 @@ import java.util.Arrays;
  */
 public class QuickSort {
     public static void main(String[] args) {
-//        int[] arr = {-9, 78, 0, 23, -567, 70};
-//        quickSort(arr, 0, arr.length - 1);
+        /*int[] arr = {-9, 78, 0, 23, -567, 70};
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));*/
         int[] arrs = new int[80000];
         for (int i = 0; i < 80000; i++) {
             arrs[i] = (int) (Math.random() * 80000);
@@ -25,24 +27,30 @@ public class QuickSort {
     }
 
     public static void quickSort(int[] arr, int left, int right) {
-        int l = left;
-        int r = right;
+        int l = left;   //左下标
+        int r = right;  //右下标
         //pivot中轴值
         int pivot = arr[(left + right) / 2];
-        int temp = 0;
+        int temp = 0;   //临时变量,用于交换操作
+        //while循环中,将比pivot大的放右边,小的放左边
         while (l < r) {
+            //左下标右移,寻找目标值
             while (arr[l] < pivot) {
                 l++;
             }
+            //右下标左移
             while (arr[r] > pivot) {
                 r--;
             }
+            //如果左右指针相遇,说明当前轮次已经排序完毕
             if (l >= r) {
                 break;
             }
+            //找到目标值后进行交换操作
             temp = arr[l];
             arr[l] = arr[r];
             arr[r] = temp;
+            //左右指针指向的值是pivot,则反向改变下标(避免与pivot无限循环交换)
             if (arr[l] == pivot) {
                 r--;
             }
@@ -50,10 +58,12 @@ public class QuickSort {
                 l++;
             }
         }
+        //如果l和r相遇,则相应移动下标值(否则会出现栈溢出异常)
         if (l == r) {
             l++;
             r--;
         }
+        //左右递归排序
         if (left < r) {
             quickSort(arr, left, r);
         }
